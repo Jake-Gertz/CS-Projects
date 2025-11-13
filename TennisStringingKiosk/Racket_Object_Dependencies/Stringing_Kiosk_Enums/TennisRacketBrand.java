591d514ac03e256b2dc2ddc4f13aa965f98837e9
Jake Dickinson <Jakedickinson@u.boisestate.edu>
2025-11-12 18:41:21 -0700

    Updated Java Docs for eumerations

--- TennisStringingKiosk/Racket_Object_Dependencies/Stringing_Kiosk_Enums/TennisRacketBrand.java
+++ TennisStringingKiosk/Racket_Object_Dependencies/Stringing_Kiosk_Enums/TennisRacketBrand.java
@@ -1,5 +1,14 @@
 package TennisStringingKiosk.Racket_Object_Dependencies.Stringing_Kiosk_Enums;
 
+/**
+ * Represents the tennis racket brands that are suported by the TennisStringingKiosk.
+ * This enum allows clear way to tell what brand of racket a user chose as well
+ * as a way to set a String description of each enumerated object.
+ * 
+ * @author Jake Gertz
+ * @date 11/12/2025
+ * @version 1.0
+ */
 public enum TennisRacketBrand {
     BABOLAT("Babolat"),
     WILSON("Wilson"),
@@ -13,10 +22,20 @@ public enum TennisRacketBrand {
 
     private final String description;
 
+    /**
+     * A constructor that allows a String description to be attached to each enumerated object.
+     * 
+     * @param description Name of the tennis brand represented by the Enumerated object
+     */
     TennisRacketBrand(String description){
         this.description = description;
     }
 
+    /**
+     * Allows the String description of each object to be retreived.
+     *
+     * @return The String description of the Enumerated object.
+     */
     @Override
     public String toString(){
         return this.description;
--- TennisStringingKiosk/Racket_Object_Dependencies/Stringing_Kiosk_Enums/TennisStringBrand.java
+++ TennisStringingKiosk/Racket_Object_Dependencies/Stringing_Kiosk_Enums/TennisStringBrand.java
@@ -1,5 +1,14 @@
 package TennisStringingKiosk.Racket_Object_Dependencies.Stringing_Kiosk_Enums;
 
+/**
+ * Represents the tennis string brands that are suported by the TennisStringingKiosk.
+ * This enum allows clear way to tell what brand of string a user chose as well
+ * as a way to set a String description of each enumerated object.
+ * 
+ * @author Jake Gertz
+ * @date 11/12/2025
+ * @version 1.0
+ */
 public enum TennisStringBrand {
     BABOLAT("Babolat"),
     WILSON("Wilson"),
@@ -16,10 +25,20 @@ public enum TennisStringBrand {
 
     private final String description;
 
+    /**
+     * A constructor that allows a String description to be attached to each enumerated object.
+     * 
+     * @param description Name of the tennis brand represented by the Enumerated object
+     */
     TennisStringBrand(String description){
         this.description = description;
     }
 
+    /**
+     * Allows the String description of each object to be retreived.
+     *
+     * @return The String description of the Enumerated object.
+     */
     @Override
     public String toString(){
         return this.description;
