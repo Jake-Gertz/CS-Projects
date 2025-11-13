80c24223d7b3dcecbe334a3bbb255b3b7194fc84
Jake Dickinson <Jakedickinson@u.boisestate.edu>
2025-11-12 18:59:35 -0700

    Started on TennisRacket.java's java docs

--- TennisStringingKiosk/Racket_Object_Dependencies/TennisRacket.java
+++ TennisStringingKiosk/Racket_Object_Dependencies/TennisRacket.java
@@ -5,6 +5,23 @@ import java.time.LocalDate;
 import TennisStringingKiosk.Racket_Object_Dependencies.Stringing_Kiosk_Enums.TennisRacketBrand;
 import TennisStringingKiosk.Racket_Object_Dependencies.Stringing_Kiosk_Enums.TennisStringBrand;
 
+/**
+ * The tennis racket class is used to represent a tennis racket object and to 
+ * hold all relevant information that would be required by a stringer or user 
+ * regarding the tennis racket. A Tennis racket has atributes of the following 
+ * denotion.
+ * 
+ * Racket brand
+ * Main string tension
+ * Cross string tension
+ * Type of tennis string for the Mains
+ * Type of tennis string for the Crosses
+ * Last date strung
+ * 
+ * @author Jake Gertz
+ * @date 11/11/2025
+ * @version 1.0
+ */
 public class TennisRacket {
     private TennisRacketBrand racketBrand;
     private int mainTension;
@@ -13,10 +30,33 @@ public class TennisRacket {
     private TennisString crossString;
     private LocalDate lastStrung;
 
+    /**
+        Finals included in class:
+
+            DEFAULT_TENSION, DEFAULT_STRING, DEFAULT_BRAND
+
+            Default tension represents the default tension you want to assign to a
+                racket if there is no provided tension value.
+
+            Default string represents the default string you want to assign to a 
+                racket if a prefered string is not specified.
+
+            Default brand represents the default tennis racket brand you want to
+                assign to a racket if no brand is specified.
+     */
     private static final int DEFAULT_TENSION = 54;
     private static final TennisString DEFAULT_STRING = new TennisString(TennisStringBrand.DEFAULT, "DEFAULT");
     private static final TennisRacketBrand DEFAULT_BRAND = TennisRacketBrand.DEFAULT;
 
+    /**
+     * This constructor allows the creation of a tennis racket object.
+     * 
+     * @param racketBrand brand of racket to assign to this object
+     * @param mainString brand of string wanted for this rackets main strings
+     * @param crossString brand of string wanted for this rackets cross strings
+     * @param mainTension tension prefered for this rackets main strings
+     * @param crossTension tension prefered for this rackets cross strings
+     */
     public TennisRacket(TennisRacketBrand racketBrand, TennisString mainString, TennisString crossString, int mainTension, int crossTension) {
         this.racketBrand = racketBrand;
         this.mainString = mainString;
@@ -25,18 +65,42 @@ public class TennisRacket {
         this.crossTension = crossTension;
     }
 
+    /**
+     * This overloaded consructor allows the creation of a racket object without a
+     * specified brand.
+     * 
+     * @param mainString brand of string wanted for this rackets main strings
+     * @param crossString brand of string wanted for this rackets cross strings
+     * @param mainTension tension prefered for this rackets main strings
+     * @param crossTension tension prefered for this rackets cross strings
+     */
     public TennisRacket(TennisString mainString, TennisString crossString, int mainTension, int crossTension) {
         this(DEFAULT_BRAND, mainString, crossString, mainTension, crossTension);
     }
 
+    /**
+     * This overloaded constructor allows the creation of a racket object with just 
+     * a single provided tension and string argument.
+     * 
+     * @param tension Tension for mains crosses 
+     * @param string Tennis strings for mains and crosses 
+     */
     public TennisRacket(int tension, TennisString string) {
         this(string, string, tension, tension);
     }
 
+    /**
+     * Allows for the creation of a tennis racket object with all default options.
+     */
     public TennisRacket() {
         this(DEFAULT_BRAND, DEFAULT_STRING, DEFAULT_STRING, DEFAULT_TENSION, DEFAULT_TENSION);
     }
 
+    /**
+     * A getter that returns the TennisRacketBrand object the racket holds
+     * 
+     * @return this rackets TennisRacketBrand
+     */
     public TennisRacketBrand getRacketBrand() {
         return racketBrand;
     }
