8df3278845e799109816b174dc74807c8d3fcb0e
Jake Dickinson <Jakedickinson@u.boisestate.edu>
2025-11-14 14:21:15 -0700

    Started TennisKioskGUI.java

---
+++ TennisStringingKiosk/TennisKioskGUI.java
@@ -0,0 +1,17 @@
+package TennisStringingKiosk;
+
+/**
+ * The TennisKioskGUI class is the driver class of the 
+ * TennisKiosk package. This GUI creates the interactable
+ * layer of the tennis kiosk, manages when to add different 
+ * objects to the TennisKiosk, tracks the current state of the 
+ * tennis kiosk, and reports useful information to either a 
+ * normal user or an admin user!
+ * 
+ * @author Jake Gertz
+ * @date 11/14/2025
+ * @version 1.0
+ */
+public class TennisKioskGUI {
+    
+}
